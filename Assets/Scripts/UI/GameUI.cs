3feeb63c140f484531d44f310791c8797d31886a
Gyeongtae Kim <lionstarsjam@gmail.com>
2022-02-05 01:24:04 +0900

    Feat: LoadingScene 추가

---
+++ Assets/Scripts/UI/GameUI.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class GameUI: MonoBehaviour {
+    public void OnClickNewGameBtn() {
+        PrintOnClickButtonName();
+        LoadingSceneManager.LoadScene("GameScene");
+    }
+
+    public void OnClickLoadGameBtn() {
+        PrintOnClickButtonName();
+    }
+
+    public void OnClickOptionsBtn() {
+        PrintOnClickButtonName();
+    }
+
+    public void OnClickExitBtn() {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+
+    void PrintOnClickButtonName() {
+        Debug.Log(EventSystem.current.currentSelectedGameObject.name + " Å¬¸¯");
+    }
+}
